00227f46d8295bbb757a91dd69817d19253bcb93
Lucas Reis <lucasreis_9@hotmail.com>
2022-08-05 17:14:13 -0300

    Finalizado o botão scrool top

--- js/script.js
+++ js/script.js
@@ -12,6 +12,12 @@ $(document).ready(function(){
     $(window).on('scroll load', function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0 ){
+            $('.top').show();
+        }else {
+            $('.top').hide();
+        }
     })
 
 
@@ -28,4 +34,4 @@ $(document).ready(function(){
     })
 
 
-});
+}); 
