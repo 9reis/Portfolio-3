50592963b43114e6422664ce3247c5559c64dc43
Lucas Reis <lucasreis_9@hotmail.com>
2022-08-05 17:01:21 -0300

    Finalizada a logica smooth, de rolagem da pagina

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
+
+/*********************************************************************************\
+                                    MENU
+\*********************************************************************************/
+
 $(document).ready(function(){
     $('#menu').click(function(){
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     });
-});
 
-$(window).on('scroll load', function(){
+    $(window).on('scroll load', function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
-})
+    })
+
+
+    // SMOOTH SCROLLING
+    
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+
+        $('html,body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },500,
+        'linear'
+        );
+    })
+
+
+});
